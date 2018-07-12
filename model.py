94b0cb8fd06df04a539b65060466aee619907eb5
Daniel <danieldeng223@gmail.com>
2018-07-12 22:35:45 +0800

    Realised that loading all the images to one giant dataframe would not work, currently working on loading 5 at a time then repeat the process.

--- model.py
+++ model.py
@@ -3,9 +3,16 @@ import tensorflow as tf
 import pandas as pd
 from PIL import Image
 
-driving_sample = pd.read_csv("~/Documents/EPQ/Mine/track1_1/driving_log.csv", sep="," , names = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Break', 'Speed'])
+#Read the images and load the data.
+driving_sample = pd.read_csv("~/Documents/EPQ/track1_1/driving_log.csv", sep="," , names = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Break', 'Speed'])
 print(driving_sample.describe())
 Center_Images = pd.Series([])
+image_data = []
+i = 0
 for index, row in driving_sample.iterrows():
-    Center_Images = Image.open(row["Center"])
+    i+=1
+    if i == 5:
+        break
+    image_data.append(Image.open(row["Center"]))
     print("loaded: " + row["Center"])
+Center_Images = pd.Series(image_data)
